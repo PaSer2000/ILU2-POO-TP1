f1715a49fef926786776083c62e9d0c73f545d52
Paul Serratos <srp4343a@ensfsi.onmicrosoft.com>
2024-03-13 10:22:59 +0100

    Tout fini sauf petite dernière question

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -11,43 +11,36 @@ public class ScenarioCasDegrade {
     public static void main(String[] args) {
     	
     	// Scénario 0: Libérer étal n’ayant pas été occupé
-    	
-//        Etal etal = new Etal(null);
-//        try {
-//            etal.libererEtal();
-//        } catch (IllegalStateException e) {
-//            System.out.println("Exception attrapée : " + e.getMessage());
-//        }
-//        System.out.println("Fin du test");
+        Etal etal0 = new Etal(null);
+        try {
+            etal0.libererEtal();
+        } catch (IllegalStateException e) {
+            System.err.println("Exception attrapée : " + e.getMessage());
+        }
         
         // Scénario 1: Acheteur null
-    	
-        System.out.println("Scénario 1: Acheteur null");
         try {
-            Etal etal = new Etal("Potion magique");
-            etal.acheterProduit(5, null);
+            Etal etal1 = new Etal("Potion magique");
+            etal1.acheterProduit(5, null);
         } catch (NullPointerException e) {
             System.err.println("Exception attrapée: " + e.getMessage());
         }
 
-        // Scénario 2: Quantité négative
-        
-        System.out.println("\nScénario 2: Quantité négative");
+        // Scénario 2: Quantité négative     
         try {
-            Etal etal = new Etal("Potion magique");
-            etal.acheterProduit(-2, new Gaulois("Astérix", 10));
+            Etal etal1 = new Etal("Potion magique");
+            etal1.acheterProduit(-2, new Gaulois("Astérix", 10));
         } catch (IllegalArgumentException e) {
             System.err.println("Exception attrapée: " + e.getMessage());
         }
 
         // Scénario 3: Étal non occupé
-        
-        System.out.println("\nScénario 3: Étal non occupé");
         try {
-            Etal etal = new Etal("Potion magique");
-            etal.acheterProduit(3, new Gaulois("Obélix", 10));
+            Etal etal1 = new Etal("Potion magique");
+            etal1.acheterProduit(3, new Gaulois("Obélix", 10));
         } catch (IllegalStateException e) {
             System.err.println("Exception attrapée: " + e.getMessage());
         }
+        
     }
 }
